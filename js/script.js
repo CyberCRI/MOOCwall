2f641f715d71b162fe985438c1174a43134dbfcb
hugolpz <hugo.lpz@gmail.com>
2017-02-06 22:24:03 +0000

    img and co

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 /* Build up gallery*/
 var tpl = {
-	project : `<div class="project container-fluid"></div>`,
-	galleries:`<div class="row galleries"></div>`,
-	gallery : `<div class="gallery col-md-4 col-xs-6"></div>`,
-	image   : `<img class="slides image" href="" />`,
-	description: `<div class="row description"></div>`,
-	modal: `<!-- Trigger the modal with a button -->
+	container:`<div class="container-fluid"></div>`,
+	galleries: `<div class="row galleries"></div>`,
+	columns : `
+		<div id="col1" class="col-md-4 col-xs-6"></div>
+		<div id="col2" class="col-md-4 hidden-sm-down"></div>
+		<div id="col3" class="col-md-4 col-xs-6"></div>`,
+	gallery : `<div class="gallery"></div>`,
+	image   : `<img class="slides image" data-toggle="modal" data-target="#mytarget" />`,
+	
+	descriptions: `<div class="row descriptions"></div>`,
+	modal: `<div class="myModal"><!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#mytarget">Open Modal</button>
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
     
-      <!-- Modal content-->
-      <div class="modal-content">
+      <div class="modal-content"><!-- Modal content-->
+
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">"Title"</h4>
+          <h4 class="title">"Title"</h4>
         </div>
         <div class="modal-body">"Content"</div>
 		<!-- <div class="modal-footer">
@@ -24,36 +29,47 @@ var tpl = {
       </div>
       
     </div>
-  </div>`
+  </div>
+</div>`
 };
 
 $(function() {
-var galleriesBuilder = function ($that, d){
-  var images = d.images,
-      modulus = Math.floor(images.length/6); // so whatever the number of images, images always fill the 6 galleries
-  $that.append(tpl.galleries);
-  console.log(images.length);
-  for(var i=0; i<images.length; i++){
-    if(i % modulus ==0 && i< modulus*6) { $that.children(".galleries").append(tpl.gallery); }
-    $that.find(".gallery").last().append(tpl.image);
-    $that.find(".image").last().attr({ src: `img/`+d.codename+`/`+d.codename+(i+1)+`.jpg`})
-  }
-  $that.find(".gallery").eq(1).addClass("hidden-sm-down");
-  $that.find(".gallery").eq(4).addClass("hidden-sm-down");
-}
-
-var modalBuilder = function($that, d) {
-	$that.append(tpl.descriptions);
 	
-}
 
-for(var p=0; p<data.length; p++){
-  var d = data[p];
-  $("#hook").append(tpl.project);
-  var that = $(".project").eq(p);
-  that.data("project", d );
-  galleriesBuilder(that, d);
+$("#hook").append(tpl.container);
+$(".container-fluid").append(tpl.galleries);
+$(".galleries").append(tpl.columns);
+
+	
+var galleriesBuilder = function (data){
+	var col1 = $("#col1"),
+		col2 = $("#col2"),
+		col3 = $("#col3");
+	for(var i=0; i<data.length; i++){
+		var d = data[i],
+			url = `img/`+d.img,
+			image = tpl.image.replace(/#mytarget/i, d.codename);
+		if(i %3==0){ col1.append(image); col1.children().last().attr("src",url) }
+		if(i %3==1){ col2.append(image); col2.children().last().attr("src",url)}
+		if(i %3==2){ col3.append(image); col3.children().last().attr("src",url)}
+	}
 }
+galleriesBuilder(images);
+
+	
+var modalBuilder = function(data) {
+	$("#hook").append(tpl.descriptions);
+	for(var d=0; d<data.length; d++){
+		$("#hook").append(tpl.modal)
+		var $that = $("#hook").children().eq(d);
+		$that.find("button").data("target", data[d].codename);
+		$that.find(".modal").attr("id", data[d].codename);
+		$that.find(".title").text(d.name);
+		$that.find(".modal-bod").html(d.html)
+	}
+}
+modalBuilder(projects);
+
 
 });
 
@@ -62,21 +78,22 @@ for(var p=0; p<data.length; p++){
 
 /* Caroussel */
 $(function() {
-$(".gallery").each(function(index) {
-var position = 1;
-var carousel = function () {
-    var i, that = $(".gallery").eq(index);
-    var x = that.children(".slides");
-    // x.children().css({"display":"none"});
-    x.css({"display":"none"});
-    x.eq(position-1).css({"display":"block"});
-    position == x.length? position = 1 : position++;
-    // if (position == x.length) { position = 0 }; position++;
+	
+	$(".gallery").each(function(index) {
+	var position = 1;
+	var carousel = function () {
+		var i, that = $(".gallery").eq(index);
+		var x = that.children(".slides");
+		// x.children().css({"display":"none"});
+		x.css({"display":"none"});
+		x.eq(position-1).css({"display":"block"});
+		position == x.length? position = 1 : position++;
+		// if (position == x.length) { position = 0 }; position++;
 
-    setTimeout(carousel, 5000); // Change image every 2 seconds
-}
-carousel();
-});
+		setTimeout(carousel, 5000); // Change image every 2 seconds
+	}
+	carousel();
+	});
 
 });
 
@@ -106,3 +123,4 @@ $(function() {
 //http://www.jsoneditoronline.org/?id=86bed24507b7689237b8eefe9498a69b
 
 /*Ask for devis */
+
