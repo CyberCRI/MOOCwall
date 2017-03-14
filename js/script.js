abf20a76094d27de0ceab7252c0d45e36d955e98
hugolpz <hugo.lpz@gmail.com>
2017-03-14 20:50:16 +0000

    new images

--- js/script.js
+++ js/script.js
@@ -16,14 +16,12 @@ var htmlHead = `
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="./img/favicon/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
-    <meta charset="utf-8">
-    <title>MOOC Factory's wall</title>
 `;
 var topbar =`
 	<ol class="breadcrumb">
 		<li class="breadcrumb-item"><a href="./index.html">
 	<img class="logo" src="./img/logo-moocfactory.svg" style="height:1.5em;" /></a></li>
-		<li class="breadcrumb-item"><a href="./index.html">MOOC Wall</a></li>
+		<li class="breadcrumb-item"><a href="./index.html">MOOCWall</a></li>
 		<li class="breadcrumb-item"><a href="./intro.html">Studio</a></li><!--
 		<li class="breadcrumb-item"><a href="./description.html">Studio de réalisation</a></li>
 		<li class="breadcrumb-item"><a href="./contact.html">Contact</a></li> -->
@@ -34,16 +32,23 @@ var sidebar = `<img class="logo" src="./img/logo-moocfactory.svg" />
 	<a href="./index.html"><div>MoocWall</div></a>
 	<a href="./intro.html"><div>Studio</div></a><!--
 	<a href="./description.html"><div>Description</div></a>
-	<div href="./">Team</div> -->
+	<div href="./">Team</div> 
+	<img class="partners" src="./img/partners/USPC-Invest-MairieParis.png" />
+	<img class="partners" src="./img/partners/IIFR.png" /> 
+	<img class="partners" src="./img/partners/cri.png" />-->
+	<img class="partners" src="./img/partners/paris.png" />
+	<img class="partners" src="./img/partners/fondation_bettencourt.png" />
 </div>
 <div class="footer">
 	<p class="icons">
 		<a target="_blank" href="https://www.youtube.com/channel/UCAwnYwPOM-fcJ7wkfp1Xn7g" title="Youtube"><i class="fa fa-youtube-play" aria-hidden="true"></i></a> 
 		<a target="_blank" href="https://vimeo.com/criparis" title="Vimeo"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 	</p>
-	<p class="authors">Mooc Producers</p>
-	<p class="authors">The CRI's MOOC Factory</p>
+	<!-- <p class="authors">CRI's MOOC Factory</p> -->
 	<p class="authors"><i class="fa fa-globe" aria-hidden="true"></i> <a target="_blank" href="http://mooc.cri-paris.org">mooc.cri-paris.org</a></p>
+<p class="contacts">Contacts :<br>
+<i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:xavier.desplas@cri-paris.org"> Production MOOC</a>
+<br><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:hugo.lopez@cri-paris.org"> Platform MOOC</a></p>
 </div>`;
 
 $("head").append(htmlHead);
@@ -57,19 +62,16 @@ var tpl = {
 	columns : `
 		<div id="col1" class="col-lg-6 col-md-6-down"></div>
 		<div id="col2" class="col-lg-6 col-md-6-down"></div>`,
-	gallery : `<div class="gallery"></div>`,
-	image   : `<img class="slides image" data-toggle="modal" data-target="#targetModal" />`,
-
-	descriptions: `<div class="row descriptions"></div>`
+	cell : `<div class="cell box"></div>`,
+	photo   : `<img class="photo" data-target="#datainfo" />`
 };
+
 
 
 /* ******************************************************************* */
 /* Data call ********************************************************* */
 /* ******************************************************************* */
-window.onload = function() {
-  init()
-};
+window.onload = function() { init() };
 
 function init() {
   Tabletop.init({
@@ -87,35 +89,50 @@ var galleriesBuilder = function (imgList){
 	for(var i=0; i<imgList.length; i++){
 		var l = imgList[i],
 			url = `img/wall2/`+l.img,
-			imageHtml = tpl.image.replace(/targetModal/i, l.codename),
+			photoHtml = tpl.photo.replace(/#datainfo/i, l.codename),
 			$col,
-			$gallery, $gallerySerie,
-			changeSerie;
+			$cell, $cellSerie,
+			chancell;
 		if(i%2==0){ $col = col1; }
 		if(i%2==1){ $col = col2; }
-		$gallery = $col.children().last();
-		$gallerySerie = $gallery.attr("serie");
-		if(true || $gallerySerie != l.codename ) { $col.append(tpl.gallery); $col.children().last().attr("serie",l.codename) }
+		$cell = $col.children().last();
+		$cellSerie = $cell.attr("serie");
+		if(true || $cellSerie != l.codename ) { $col.append(tpl.cell); $col.children().last().attr("serie",l.codename) }
 		console.log(l.codename)
-		$gallery = $col.children().last();
-		$gallery.append(imageHtml);
-		$gallery.children().last().attr("src",url);
+		$cell = $col.children().last();
+		$cell.append(photoHtml);
+		$cell.children().last().attr("src",url);
 
 		currentSerie = l.codename;
-		serieCounter++; ///           count so we get 5 images max per gallery !
+		serieCounter++; ///           count so we get 5 images max per cell !
 	}
 }
 
 /* Modale ****************************************************************** */
-var contentHtml = function (d) {
-	var sessionStr = d.session?" ("+d.session+")":"",
-		urlStr = `Homepage : <a href="`+d.url+`">click here</a> `+ sessionStr,
-		categoryStr = `Categories : `+d.categories,
-		organisationStr = `Organization.s / Sponsor.s: `+d.organisation,
-		periodStr = `Period : `+d.start+`→`+d.end,
-		learnersStr = `Impact : `+ d.participants+` trainees/students`,
-		iframeStr = d.url?`<iframe frameborder=0 scrolling=no style='width:100%; height:400px;' src="`+d.url+`"></iframe>`:"",
-		videoStr = d.introVideoIframeSrc?`<iframe frameborder="0" width="100%" data-height="270" src="`+d.introVideoIframeSrc+`" allowfullscreen></iframe>`:"";
+var infoboxHtml = function (d) {
+	var buildInfobox = function (d){
+	console.log(d);
+	return `<div class="row infobox">
+			<div class="col-md-6 col-xs-12">
+			<h4>`+d.fullname+`</h4>
+			<ul>
+				<li>Homepage : <a href="`+d.url+`">click here</a> `+ (d.session?" ("+d.session+")":"") +`</li>
+				<li>On `+ d.categories +`</li>
+				<li>Thanks to `+ d.organisation +`</li>
+				<li>Open on `+d.start+`→`+d.end+`</li>
+				<li>Impact : `+ d.participants+` trainees</li>
+			</ul>
+		</div>
+
+		<div class="col-md-6 col-xs-12 iframe">`+(d.url?`<iframe frameborder=0 scrolling=no src="`+d.url+`"></iframe>`:"")+`</div>
+		<!--`		
+		+(d.introVideoIframeSrc?`
+			<div class="col-xs-12"><iframe frameborder="0" src="`+d.introVideoIframeSrc+`" allowfullscreen></iframe>
+		</div>`:"")+
+
+	`--></div>`;
+}
+	var infobox = buildInfobox(d);
 
 	var	modal = `<div class="modal fade" id="`+d.codename+`" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
 			<div class="modal-dialog modal-lg">
@@ -125,65 +142,59 @@ var contentHtml = function (d) {
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					  <span aria-hidden="true">×</span></button>
 					</div>
-					<div class="modal-body">
-						<div class="row">
-							<div class="col-md-6 col-xs-12">
-								<b>Few elements:</b>
-								<ul>
-									<li>`+ urlStr +`</li>
-									<li>`+ categoryStr +`</li>
-									<li>`+ organisationStr +`</li>
-									<li>`+ periodStr +`</li>
-									<li>`+ learnersStr +`</li>
-								</ul></div>
-							<div class="col-md-6 col-xs-12">`+iframeStr+`</div>
-						</div>
-						<div class="row">
-					<!-- <div class="col-xs-2"></div><div class="col-xs-8">`+videoStr+`</div><div class="col-xs-2"></div> -->
-						</div>
-					</div>
+					<div class="modal-body">`
+						+infobox+
+					`</div>
 				</div>
 			</div>
 		</div>`;
-	return modal;
+	return infobox;
 }
-var modalBuilder = function(data) {
-	$("#hook").append(tpl.descriptions);
+
+var infoboxBuilder = function(data) {
 	for(var i=0; i<data.length; i++){
 		var project = data[i],
-			html = contentHtml(project)
-		$(".descriptions").append(html)
+			html = infoboxHtml(project);
+		$('*[data-target="'+project.codename+'"]').parent().append(html)
 	}
+	
 }
 
 var carousselBuilder = function () {
-	$(".gallery").each(function(index) {
+	$(".slider").each(function(index) {
 	var position = 1;
 	var carousel = function () {
-		var i, that = $(".gallery").eq(index);
-		var x = that.children(".slides");
+		console.log("hi")
+		var i, that = $(".slider").eq(index);
+		var x = that.children(".photo");
 		// x.children().css({"display":"none"});
-		x.css({'display':'none', 'visibility': 'hidden', 'opacity': '0'});
-		x.eq(position-1).css({'display':'block', 'visibility': 'visible','opacity': '1'});
+		x.css({'display':'none', 'opacity': '0'});
+		x.eq(position-1).css({'display':'block', 'opacity': '1'});
 		position == x.length? position = 1 : position++;
 		// if (position == x.length) { position = 0 }; position++;
-		setTimeout(carousel, 2000); // Change image every 2 seconds
+		setTimeout(carousel, 12000); // Change image every 2 seconds
 	}
 	carousel();
 	});
  }
 
 var moocWall = function (data, tabletop) {
-	console.log(JSON.stringify(data))
-	$("#hook").addClass("container-fluid wall2");
-	$(".wall2").append(tpl.galleries);
+	// console.log(JSON.stringify(data))
+	// build structure
+	$(".wall").addClass("container-fluid")
+		.append(tpl.galleries);
 	$(".galleries").append(tpl.columns);
-
+	// fill the structure
 	galleriesBuilder(wall2);
-	modalBuilder(data);
+	infoboxBuilder(data);
 	carousselBuilder();
-
 };
+
+
+
+
+
+
 
 
 /* Caroussel */
